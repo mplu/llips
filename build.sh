a5aa3a32cbb038acc7304b8f8045a38837cc2c93
Matthieu <m.pluvinage@gmail.com>
2014-03-11 09:16:43 +0100

    change build script for linux compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rm BMP_Compare
-gcc BMP_Compare.c functions.c lib_img.c motiondetect.c motorcontrol.c -o BMP_Compare
+gcc BMP_Compare.c functions.c lib_img.c motiondetect.c motorcontrol.c contrastdetect.c -o BMP_Compare
