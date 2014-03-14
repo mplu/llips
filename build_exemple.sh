3682055928632bb8be7d86b69aba0f35c8b6e5cd
Matthieu <m.pluvinage@gmail.com>
2014-03-14 16:04:04 +0100

    correction on linux building script

--- build_exemple.sh
+++ build_exemple.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rm llips_exemple
-gcc llips_exemple.c lipps/llips_contrastdetect.c lipps/llips_functions.c lipps/llips_general.c lipps/llips_io.c lipps/llips_motiondetect.c -o llips_exemple
+gcc llips_exemple.c llips/llips_contrastdetect.c llips/llips_functions.c llips/llips_general.c llips/llips_io.c llips/llips_motiondetect.c -o llips_exemple
